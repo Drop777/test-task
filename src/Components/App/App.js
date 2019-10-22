5d99b4bfc9c7dc673c9b11bae590d9a79011d7b5
Pukhnichenko <victorpuhnichenko@gmail.com>
2019-10-22 22:29:01 +0300

    'fix-gh-page'

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/prefer-stateless-function */
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import { Route, Switch } from 'react-router';
@@ -5,14 +6,18 @@ import { Users } from '../Users/index';
 import { Posts } from '../Posts/index';
 import { Details } from '../Details/index';
 
-const App = () => (
-  <>
-    <Switch>
-      <Route path="/:userId?/posts/:postId?/details" render={(match) => <Details match={match} />} />
-      <Route path="/:userId?/posts" render={(match) => <Posts match={match} />} />
-      <Route path="/" component={Users} />
-    </Switch>
-  </>
-);
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        <Switch>
+          <Route path="/:userId?/posts/:postId?/details" render={(match) => <Details match={match} />} />
+          <Route path="/:userId?/posts" render={(match) => <Posts match={match} />} />
+          <Route path="/" component={Users} />
+        </Switch>
+      </>
+    );
+  }
+}
 
 export default App;
