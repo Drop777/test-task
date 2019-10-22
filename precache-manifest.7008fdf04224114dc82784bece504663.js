06364faeafd23522c143423fe4f44485dd1b7196
Pukhnichenko <victorpuhnichenko@gmail.com>
2019-10-22 21:58:31 +0300

    Updates

---
+++ precache-manifest.7008fdf04224114dc82784bece504663.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86345d90ab826bc057d65913b3c852a9",
+    "url": "/test-task/index.html"
+  },
+  {
+    "revision": "c3f7134fa547d2241220",
+    "url": "/test-task/static/css/main.9a180505.chunk.css"
+  },
+  {
+    "revision": "e0ce3014a3bc32c1bffe",
+    "url": "/test-task/static/js/2.d9c0a28c.chunk.js"
+  },
+  {
+    "revision": "c3f7134fa547d2241220",
+    "url": "/test-task/static/js/main.0cd5261f.chunk.js"
+  },
+  {
+    "revision": "6fd7e9e19b8cb3e935d6",
+    "url": "/test-task/static/js/runtime-main.9d7e0466.js"
+  }
+]);
